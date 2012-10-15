faf5d4948e23e34c8425c19d5487d5c88e34fc04
Cristian Planas <me@cristianplanas.com>
2012-10-15 20:49:16 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetmovies::Application.config.secret_token = 'b317bc5a12b7066b60c06ecb02bd2b1d97e31c5aa44ab25de4dd2b64efa852bc0f86fc427f73a81b9b811c145edb52327acbcc2c04f67d9c3d46c44cc3aafb33'
