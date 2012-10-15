683ac1d10b4ddbe85cb9f47e723bcbe23364e50c
Cristian Planas <me@cristianplanas.com>
2012-10-16 00:33:59 +0200

    Adding basic validations

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,4 +2,6 @@ class Keyword < ActiveRecord::Base
   attr_accessible :movie_id, :word
 
   belongs_to :movie
+
+  validates_presence_of :word
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ActiveRecord::Base
   attr_accessible :title
 
   has_many :keywords
+
+  validates_presence_of :title
 end
